a1db34cab145811622d4df6b5062b479e69c2987
John Burns <johnb1003@gmail.com>
2022-10-19 23:14:38 -0600

    fix test

--- src/test/java/com/eatthefrog/GoalService/GoalServiceApplicationTests.java
+++ src/test/java/com/eatthefrog/GoalService/GoalServiceApplicationTests.java
@@ -3,6 +3,8 @@ package com.eatthefrog.GoalService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
@@ -10,6 +12,12 @@ class GoalServiceApplicationTests {
 
 	@MockBean
 	private JwtDecoder jwtDecoder;
+
+	@MockBean
+	private ClientRegistrationRepository clientRegistrationRepository;
+
+	@MockBean
+	private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
 
 	@Test
 	void contextLoads() {
