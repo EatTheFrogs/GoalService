da5f33ffa03ddba896ceed8bc129530634206110
John Burns <johnb1003@gmail.com>
2022-10-27 02:31:09 -0600

    Update MongoConfig.java

--- src/main/java/com/eatthefrog/GoalService/config/MongoConfig.java
+++ src/main/java/com/eatthefrog/GoalService/config/MongoConfig.java
@@ -5,6 +5,8 @@ import com.eatthefrog.GoalService.converter.DateWritingConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
 import java.util.Arrays;
